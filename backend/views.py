89e7de3aee6e5ba4c86e008dfb196bd6c4fbcce6
SatkarNiraula <satkarniraula69@gmail.com>
2024-03-11 20:35:49 +0545

    fresh start

--- backend/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def dashboard(request):
-    return render(request, "backend/index.html")
-
-def banner(request):
-    return render(request, "backend/banner.html")
